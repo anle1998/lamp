dde6bb8a08772de02449d6c35315f2d664f5a4e8
anle1998 <765894325@qq.com>
2019-01-17 09:36:08 +0800

    2019-1-17 实验 shiyan.php

---
+++ shiyan.php
@@ -0,0 +1,25 @@
+<?php 
+	
+	// for ($i=0; $i <9 ; $i++) { 
+	// 	for ($j=0; $j < $i ; $j++) { 
+	// 		echo '*';
+	// 	}
+	// 	echo '<br>';
+	// }
+	// // echo phpinfo();
+
+
+
+
+	// $str = 'http://www.aaa.com/sss/ddd/pass.jpeg';
+
+	// $houzhui = trim(strrchr($str,'.'),'.');
+	// echo $houzhui;
+
+
+	
+
+	echo json_encode($_GET);
+
+
+?>
